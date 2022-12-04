993b6dd46294b9d2c0df2fa4bb0160739cf26fcb
Sanchay Kumar <sanchaykumar9@gmail.com>
2022-12-04 16:13:36 +0530

    Add Day 4

---
+++ day4/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...)
+#endif
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    string s;
+
+    int ans = 0;
+    while (cin >> s) {
+        string first = s.substr(0, s.find(','));
+        string second = s.substr(s.find(',') + 1);
+
+        int l1 = stoll(first.substr(0, first.find('-'))), r1 = stoll(first.substr(first.find('-') + 1));
+        int l2 = stoll(second.substr(0, second.find('-'))), r2 = stoll(second.substr(second.find('-') + 1));
+
+        if ((l1 <= l2 && r1 >= r2) || (l2 <= l1 && r2 >= r1)) {
+            ++ans;
+        }
+    }
+
+    cout << ans << '\n';
+}
