e6ec76f0c7009c2ee4a3501e22deb9e112d8bc87
Sanchay Kumar <sanchaykumar9@gmail.com>
2022-12-07 01:22:06 +0530

    Add Day 6

---
+++ day6/a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...)
+#endif
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+    string s; cin >> s;
+    int n = s.size();
+
+    for (int i = 3; i < n; i++) {
+        set<char> st;
+
+        for (int j = i; j >= i - 3; j--) {
+            st.insert(s[j]);
+        }
+
+        if (st.size() == 4) {
+            cout << i + 1 << '\n';
+            return 0;
+        }
+    }
+}
