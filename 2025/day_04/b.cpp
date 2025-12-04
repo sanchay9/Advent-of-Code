81a867a7a07c3079753e0b3ce8d8e96878ab1f81
sanchaykumar <sanchaykumar9@gmail.com>
2025-12-04 18:41:30 +0530

    add day 2

--- 2025/day_04/b.cpp
+++ 2025/day_04/b.cpp
@@ -24,7 +24,7 @@ int32_t main() {
     }
 
     int n = static_cast<int>(G.size()), m = static_cast<int>(G[0].size());
-    int64_t ans = 0;
+    queue<pair<int, int>> q;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (G[i][j] == '.') {
@@ -39,9 +39,40 @@ int32_t main() {
                     ++cnt;
                 }
             }
+
             if (cnt < 4) {
-                ++ans;
-                G[i][j] = '.';
+                q.emplace(i, j);
+            }
+        }
+    }
+
+    int64_t ans = 0;
+    while (!q.empty()) {
+        auto [i, j] = q.front();
+        q.pop();
+
+        if (G[i][j] == '.') {
+            continue;
+        }
+
+        G[i][j] = '.';
+        ++ans;
+
+        for (auto [di, dj] : directions) {
+            int ni = i + di, nj = j + dj;
+            if (ni >= 0 && ni < n && nj >= 0 && nj < m && G[ni][nj] != '.') {
+                int cnt = 0;
+                for (auto [ddi, ddj] : directions) {
+                    int nni = ni + ddi, nnj = nj + ddj;
+                    if (nni >= 0 && nni < n && nnj >= 0 && nnj < m &&
+                        G[nni][nnj] == '@') {
+                        ++cnt;
+                    }
+                }
+
+                if (cnt < 4) {
+                    q.emplace(ni, nj);
+                }
             }
         }
     }
